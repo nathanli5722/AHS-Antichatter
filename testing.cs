c2a1cc1dcf105fb2aa9579b6ee40164a374d258c
nathanli5722 <152818045+nathanli5722@users.noreply.github.com>
2024-08-31 13:00:53 -0400

    Add files via upload

---
+++ testing.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Numerics;
+using OpenTabletDriver.Plugin.Attributes;
+using OpenTabletDriver.Plugin.Tablet;
+using OpenTabletDriver.Plugin.Output;
+
+
+//ignore most of the stuff, its for compatibility lol
+namespace ANTICHATTER
+{
+    [PluginName("ANTICHATTER")]
+    public class Antichatter : IPositionedPipelineElement<IDeviceReport>
+    {
+        //example property
+        /*
+        [Property("<name>"), DefaultPropertyValue(6f), ToolTip
+            ("<description>")]
+        public float variable { set; get; }
+        */
+
+        private Vector2 LastPos;
+        //variables
+
+        public Vector2 Filter(Vector2 input)
+        {
+
+            //example function (stores input as lastPos for next Filter call)
+
+
+            
+            LastPos = input;
+            return LastPos;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+        public event Action<IDeviceReport> Emit;
+
+        public void Consume(IDeviceReport value)
+        {
+            if (value is ITabletReport report)
+            {
+                report.Position = Filter(report.Position);
+                value = report;
+            }
+
+            Emit?.Invoke(value);
+        }
+        
+        public PipelinePosition Position => PipelinePosition.PreTransform;
+
+        
+    }
+}
